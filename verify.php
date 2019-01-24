14fd12d601397b2e3a95f3ddc9f188f382638002
Somruethai58160687 <36035186+Somruethai58160687@users.noreply.github.com>
2019-01-24 15:53:46 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'rEgiNscRd2tj6qYik5nXO2+sKc+59c3lzPCh0ZmhOBVQgXQVJ85Zq3SRzbygv8fg8ch08qpVg/U+Dxqy/cPUeg/6t9Rt0vCBe7mKNGMqcfoCLthmb3bH6RJFnhClJQLY5BNIY6NqRoywsjbll1y7TQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
